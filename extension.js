eab576f31cc0a69c83f6714a81fc32f7f74e1b33
Bailey Liang <bingjetli88@gmail.com>
2022-11-16 18:35:13 -0330

    iterdev

--- extension.js
+++ extension.js
@@ -1,35 +1,13 @@
 'use strict';
-const ExtensionUtils = imports.misc.extensionUtils;
-const Shell = imports.gi.Shell;
-
-const this_extension = ExtensionUtils.getCurrentExtension(); 
-const shell_global = Shell.global.get();
-
-
-const onWindowCreate = window => {
-    //set transparency
-    window.set_opacity(128);
-};
-
-
 function init() {
-    log(`initializing ${this_extension.metadata.name}`);
 }
 
 function enable() {
-    log(`enabling ${this_extension.metadata.name}`);
-
-    //setup event-listeners
-    //window_created_handler = current_display.connect('window-created', onWindowCreate);
-
-    const pointer = Shell.Global.get_pointer();
-    log(`pointer x : ${pointer.x} pointer y : ${pointer.y}`);
+    const window_actors = global.get_window_actors();
+    window_actors.forEach(actor => {
+        actor.set_opacity(128);
+    });
 }
 
 function disable() {
-    log(`disabling ${this_extension.metadata.name}`);
-
-
-    //remove event-listeners
-    //current_display.disconnect(window_created_handler);
 }
