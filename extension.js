b4489163d3dadd2bef13aee3f9339a2b14486686
Bailey Liang <bingjetli88@gmail.com>
2022-11-17 17:52:01 -0330

    iterdev

--- extension.js
+++ extension.js
@@ -1,28 +1,52 @@
 'use strict';
 
-const Shell = imports.gi.Shell;
-const Meta = imports.gi.Meta;
-const Clutter = imports.gi.Clutter;
+//=========================================================
+// Global Variables (immutable)
+//=========================================================
+const OPACITY = 255 * 0.85;
 
+//=========================================================
+// Global Variables (mutable)
+//=========================================================
+let window_created_handler = null;
+
+//=========================================================
+// Helper Functions
+//=========================================================
+const onWindowCreated = meta_window => {
+    log(meta_window);
+    //const actor = meta_window.get_compositor_private();
+    //actor.get_children()[0].set_opacity(OPACITY);
+};
+
+const setOpacityForAllWindows = opacity => {
+    const window_actors = global.get_window_actors();
+    window_actors.forEach(w => {
+        const window_title = w.get_meta_window().get_title();
+        if(window_title !== 'gnome-shell'){
+            w.get_children()[0].set_opacity(opacity);
+        }
+    });
+};
+
+//=========================================================
+// Extension Entry Points
+//=========================================================
 function init() {
 }
 
 function enable() {
-    const window_actors = global.get_window_actors();
-    window_actors.forEach(w => {
-        const window_title = w.get_meta_window().get_title();
-        if(window_title !== 'gnome-shell'){
-            w.set_opacity(128);
-        }
-    });
+    //set opacity for existing windows
+    setOpacityForAllWindows(OPACITY);
+
+    //setup window-created event-listener
+    window_created_handler = global.display.connect('window-created', onWindowCreated);
 }
 
 function disable() {
-    const window_actors = global.get_window_actors();
-    window_actors.forEach(w => {
-        const window_title = w.get_meta_window().get_title();
-        if(window_title !== 'gnome-shell'){
-            w.set_opacity(255);
-        }
-    });
+    //reset opacity
+    setOpacityForAllWindows(255);
+
+    //remove window-created event-listener
+    global.display.disconnect(window_created_handler);
 }
